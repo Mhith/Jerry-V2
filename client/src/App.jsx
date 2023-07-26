effa9cbb6b24bc36774b674280d10f1e8f4d8c97
Mhith <dev.mhith@gmail.com>
2023-07-26 18:31:05 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import { logo } from './assets';
+import { Home, CreatePost } from './pages';
+
+const App = () => (
+  <BrowserRouter>
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+      <Link to="/">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+      </Link>
+
+      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
+    </header>
+    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+    </main>
+    <footer className="px-4 py-1 w-full bg-[#dddfe6]">
+      <div className="flex justify-between items-center py-4">
+        <p className="font-inter font-medium text-[#6469ff]">Jerry AI © 2023</p>
+        <p className="font-inter font-medium text-[#6469ff]">All rights reserved</p>
+      </div>
+    </footer>
+  </BrowserRouter>
+);
+
+export default App;
